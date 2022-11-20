6bd074355b7cb3352157f52a6c13a482a05cfc26
Lunae Somnia <lunaesomnia@protonmail.com>
2022-11-20 23:07:11 +0100

    using scss

---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div class="header">
+  <p>Hello there!</p>
+</div>
+
+<style lang="scss">
+  .header {
+    background-color: red;
+
+    > p {
+      color: green;
+    }
+  }
+</style>
